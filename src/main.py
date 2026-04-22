775bc857eb80d9d925213ae82a79bef23789c373
gmandres10 <gman3david@gmail.com>
2026-04-22 19:52:51 +0000

    Apr 22, 2026, 3:52 PM

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import os
+
+
+APP_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_data_path(filename: str) -> str:
+    '''Returns the path to an asset file, given its filename.'''
+    return os.path.join(APP_PATH, "data", filename)
+
