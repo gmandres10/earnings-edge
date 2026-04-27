83fdfc3277ff5c1e74f91108f3ae72c5a708583b
gmandres10 <gman3david@gmail.com>
2026-04-27 04:05:40 +0000

    Apr 27, 2026, 12:05 AM

--- src/earnings_analyzer.py
+++ src/earnings_analyzer.py
@@ -8,5 +8,5 @@ class EarningsAnalyzer:
         
     def _clean(self, df: pd.DataFrame):
         df = df.dropna(subset=["EPS Estimate", "Reported EPS"]).copy()
-        df = df.sort_index
+        df = df.sort_index(ascending = True)
     
