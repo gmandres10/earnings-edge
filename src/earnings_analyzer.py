3ce6d13849f171c85e0d977c7fbc132fd55f6f3f
gmandres10 <gman3david@gmail.com>
2026-04-27 04:01:26 +0000

    Apr 27, 2026, 12:01 AM

--- src/earnings_analyzer.py
+++ src/earnings_analyzer.py
@@ -3,4 +3,7 @@ import pandas as pd
 class EarningsAnalyzer:
     
     def __init__(self, raw_df, num_quarters = 4):
-        self.
+        self.num_quarters = num_quarters
+        self.df = self._clean(raw_df)
+        
+    
