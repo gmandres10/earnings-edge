05cd404fc09d7d8ab058e0d228baa7a219743709
gmandres10 <gman3david@gmail.com>
2026-05-04 01:07:18 +0000

    May 3, 2026, 9:07 PM

--- src/earnings_analyzer.py
+++ src/earnings_analyzer.py
@@ -2,13 +2,7 @@ import pandas as pd
 
 class EarningsAnalyzer:
     
-    def __init__(self, raw_df, num_quarters = 4):
-        self.num_quarters = num_quarters
-        self.df = self._clean(raw_df)
+    def __init__(self, raw_df):
+        self.df = self.
         
-    def _clean(self, df: pd.DataFrame):
-        df = df.dropna(subset=["EPS Estimate", "Reported EPS"]).copy()
-        df = df.sort_index(ascending = True)
-        
-        df = df.tail(self.num_quarters)
     
