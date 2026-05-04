77a2be9111bb405e9bf8ed4cc985c8b430efe1d6
gmandres10 <gman3david@gmail.com>
2026-05-04 02:29:09 +0000

    May 3, 2026, 10:29 PM

--- src/main.py
+++ src/main.py
@@ -77,7 +77,8 @@ else:
     
     st.write("### EPS Surprise % Per Quarter")
     
-    fig, ax = plt.sup
+    fig, ax = plt.subplots(figsize=(12,5))
+    
     
     st.write("### Earnings History")
     st.dataframe(ss.df, use_container_width=True)
