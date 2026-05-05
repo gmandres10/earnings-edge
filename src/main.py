22216b338a10e9851677604f3fd15e854d8c9e88
gmandres10 <gman3david@gmail.com>
2026-05-05 15:33:43 +0000

    May 5, 2026, 11:33 AM

--- src/main.py
+++ src/main.py
@@ -73,6 +73,10 @@ else:
     col3.metric("Best Quarter", f"{ss.analyzer.best_quarter():+.1f}%")
     col4.metric("Worst Quarter", f"{ss.analyzer.worst_quarter():+.1f}%")
     
+    
+    
+    
+    
     st.divider()
     
     st.write("### EPS Surprise % Per Quarter")
